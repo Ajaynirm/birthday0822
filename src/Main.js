81c01c640c30603b28c4eca2f406d9038694b828
Ajaynirm <ajay@AJ-MacBook-Air-M1.local>
2024-06-07 15:24:08 +0530

    second commit

--- src/Main.js
+++ src/Main.js
@@ -44,7 +44,7 @@ const Main = () => {
       const [timeExceeded, setTimeExceeded] = useState(false);
 
       // Set target time to June 7th at 23:00
-      const targetTime = new Date(new Date().getFullYear(), 5, 7, 15, 30, 0);
+      const targetTime = new Date(new Date().getFullYear(), 5, 7, 23, 59, 59);
     
       useEffect(() => {
         const checkTime = setInterval(() => {
